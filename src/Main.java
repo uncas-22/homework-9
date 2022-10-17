a6367fa332f183fe66e9158c1d77b2360513682c
Alex <seredin-alex@mail.ru>
2022-10-17 20:46:14 +0300

    Выполнил задачу 3. Исправил ошибки в задаче 4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Урок 9. Задача 1.1");
+        System.out.println("Урок 9. Массивы. Задача 1.1");
         int [] array = new int [3];
         array [0] = 1;
         array [1] = 2;
@@ -34,7 +34,35 @@ public class Main {
         }
         System.out.println();
         System.out.println("Урок 9. Задача 1.3 - в обратном порядке");// в обратном порядке
-
+        for (int i = array.length - 1; i >= 0 ; i--) {
+            System.out.print(array[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = arraySecond.length - 1; i >= 0 ; i--) {
+            System.out.print(arraySecond[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = arrayThird.length - 1; i >= 0 ; i--) {
+            System.out.print(arrayThird [i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
         System.out.println("Урок 9. Задача 1.4 - четные числа");//четные числа
+        int [] array2 = {1, 2, 3};
+        for (int d = 0; d < array2.length; d++) {
+            System.out.print(" ");
+          if (array2[d] % 2 != 0) {
+              array2[d]++;
+          }
+            System.out.print(array2 [d]);
+        }
     }
 }
